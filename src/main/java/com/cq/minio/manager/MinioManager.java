958735e5d66e88d7e6332fb3ea7defae00e947f1
cq2021-coder <2972084238@qq.com>
2024-05-30 22:17:10 +0800

    处理捕获异常

--- src/main/java/com/cq/minio/manager/MinioManager.java
+++ src/main/java/com/cq/minio/manager/MinioManager.java
@@ -26,52 +26,43 @@ public class MinioManager {
 
     /**
      * 上传对象
-     * todo 异常需要用 SDK 自定义异常兜住
      *
      * @param key  key
      * @param file 文件
      * @return {@link ObjectWriteResponse }
-     * @throws IOException               IOException
-     * @throws ServerException           服务器异常
-     * @throws InsufficientDataException 数据不足异常
-     * @throws ErrorResponseException    错误响应异常
-     * @throws NoSuchAlgorithmException  无此算法异常
-     * @throws InvalidKeyException       无效密钥异常
-     * @throws InvalidResponseException  无效响应异常
-     * @throws XmlParserException        xml分析器异常
-     * @throws InternalException         内部异常
      */
-    public ObjectWriteResponse uploadObject(String key, File file) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
-                .bucket(minioConfig.getBucketName())
-                .object(key)
-                .filename(file.getAbsolutePath())
-                .build();
-        log.info("文件访问地址：{}", minioConfig.getEndpoint() + "/" + minioConfig.getBucketName() + "/" + key);
-        return minioClient.uploadObject(uploadObjectArgs);
+    public ObjectWriteResponse uploadObject(String key, File file) {
+        try {
+            UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
+                    .bucket(minioConfig.getBucketName())
+                    .object(key)
+                    .filename(file.getAbsolutePath())
+                    .build();
+            return minioClient.uploadObject(uploadObjectArgs);
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
+                 XmlParserException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * 删除对象
-     * todo 异常需要用 SDK 自定义异常兜住
      *
      * @param key key
-     * @throws ServerException           服务器异常
-     * @throws InsufficientDataException 数据不足异常
-     * @throws ErrorResponseException    错误响应异常
-     * @throws IOException               IOException
-     * @throws NoSuchAlgorithmException  无此算法异常
-     * @throws InvalidKeyException       无效密钥异常
-     * @throws InvalidResponseException  无效响应异常
-     * @throws XmlParserException        xml分析器异常
-     * @throws InternalException         内部异常
      */
-    public void removeObject(String key) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
-                .bucket(minioConfig.getBucketName())
-                .object(key)
-                .build();
-        minioClient.removeObject(removeObjectArgs);
+    public void removeObject(String key) {
+        try {
+            RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
+                    .bucket(minioConfig.getBucketName())
+                    .object(key)
+                    .build();
+            minioClient.removeObject(removeObjectArgs);
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
+                 XmlParserException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
